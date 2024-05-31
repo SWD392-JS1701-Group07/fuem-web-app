ed8affcba0bc6ecc896e730babb5cb68cbc6a768
masaru05 <azusa9002@gmail.com>
2024-05-31 22:11:27 +0700

    ♻️ Refactor component imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 
 import { Route, Routes } from "react-router-dom";
-import EventList from "./pages/event/EventList";
-import HomePages from "./pages";
+import EventList from "@/pages/event/EventList";
+import HomePage from "@/pages/home/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePages />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/event" element={<EventList />} />
       </Routes>
     </>
