ab7f7213c234e8b3e2ee365672b6fe090edaa81c
masaru05 <azusa9002@gmail.com>
2024-05-31 22:09:35 +0700

    🔧 Add some custom tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ export default {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      jura: ['Jura', 'sans-serif']
+    },
     container: {
       center: true,
       padding: "2rem",
@@ -17,6 +21,12 @@ export default {
       },
     },
     extend: {
+      backgroundImage: {
+        'home-dots': "url('http://ali.sandbox.etdevs.com/virtual-conference/wp-content/uploads/sites/21/2021/05/virtual-conference-31.png')",
+        'column-blob': "url(http://ali.sandbox.etdevs.com/virtual-conference/wp-content/uploads/sites/21/2021/05/virtual-conference-36.png)",
+        'column-dots': "url(http://ali.sandbox.etdevs.com/virtual-conference/wp-content/uploads/sites/21/2021/04/virtual-conference-03.png)",
+        'footer': "url(http://ali.sandbox.etdevs.com/virtual-conference/wp-content/uploads/sites/21/2021/04/virtual-conference-07.png)"
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
