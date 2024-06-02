7c5d786149fa5914e49b195ba3826cec4a1702cf
masaru05 <azusa9002@gmail.com>
2024-06-02 22:02:14 +0700

    🎨 Improve code structure of main header

--- src/components/layout/header/mainHeader.tsx
+++ src/components/layout/header/mainHeader.tsx
@@ -1,39 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+
+const navLinks = [
+  { name: 'About', path: '#' },
+  { name: 'Events', path: '/event' },
+  { name: 'Contact', path: '#' },
+  { name: 'Login', path: '#' }
+]
 
 const MainNavBar = () => {
-    return (
-        <div>
-            <header className="z-50 w-full bg-white">
-                <div
-                    id="navbar-container"
-                    className="relative m-auto flex w-full max-w-full items-center justify-between px-10 py-6 text-left"
-                >
-                    <div id="title-container">
-                        <h1 className="font-poppins text-3xl font-semibold text-gray-900">
-                            <a href="#">Event Management</a>
-                        </h1>
-                    </div>
-                    <div id="nav">
-                        <ul className="flex space-x-6">
-                            <li className="font-poppins font-medium">
-                                <a href="#">About</a>
-                            </li>
-                            <li className="font-poppins font-medium">
-                                <Link to="/event">
-                                    <a href="/event">Events</a>
-                                </Link>
-                            </li>
-                            <li className="font-poppins font-medium">
-                                <a href="#">Contact</a>
-                            </li>
-                            <li className="font-poppins font-medium">
-                                <a href="#">Login</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </header>
+  return (
+    <div>
+      <header className="z-50 w-full bg-white text-black">
+        <div
+          id="navbar-container"
+          className="relative m-auto flex w-full max-w-full items-center justify-between px-10 py-6 text-left"
+        >
+          <div id="title-container">
+            <h1 className="font-poppins text-3xl font-semibold">
+              <a href="#">Event Management</a>
+            </h1>
+          </div>
+          <div id="nav">
+            <ul className="flex space-x-6">
+              {navLinks.map((link) => (
+                <li key={link.name} className="font-poppins font-medium">
+                  <Link to={link.path}>
+                    <a href={link.path}>{link.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-    )
+      </header>
+    </div>
+  )
 }
-export default MainNavBar;
+
+export default MainNavBar
