814ffeaf034ae556e92d633d91201bd45073e1d8
masaru05 <azusa9002@gmail.com>
2024-06-06 22:44:38 +0700

    ✨ Update app routers and layout logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { Route, Routes } from "react-router-dom";
-import EventList from "./pages/event/EventList";
-import { DashboardLayout, LandingPageLayout } from "./components/layout/layout";
-import HomePage from "./pages/home/HomePage";
-import DashBoard from "./pages/dashboard";
+import { Route, Routes } from 'react-router-dom'
+import EventList from './pages/event/EventList'
+import { DashboardLayout, LandingPageLayout } from './components/layout/layout'
+import HomePage from './pages/home/HomePage'
+import DashBoard from './pages/dashboard'
+import LoginPage from './pages/home/LoginPage'
+import SignupPage from './pages/home/SignupPage'
 
 function App() {
   return (
@@ -10,14 +12,16 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPageLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="dashboard" element={<DashboardLayout />} >
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="dashboard" element={<DashboardLayout />}>
             <Route path="" element={<DashBoard />} />
             <Route path="event" element={<EventList />} />
           </Route>
         </Route>
       </Routes>
     </>
-  );
+  )
 }
 
 export default App
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,30 +1,32 @@
-import { Outlet } from "react-router-dom";
-import MainNavBar from "./header/mainHeader";
-import Siderbar from "./sidebar/sideBar";
-import MainFooter from "./footer/mainFooter";
-import DashBoardFooter from "./footer/dashboardFooter";
-import DashBoardNavBar from "./header/dashboardHeader";
+import { Outlet, useLocation } from 'react-router-dom'
+import MainNavBar from './header/mainHeader'
+import Siderbar from './sidebar/sideBar'
+import MainFooter from './footer/mainFooter'
+import DashBoardFooter from './footer/dashboardFooter'
+import DashBoardNavBar from './header/dashboardHeader'
 
 const DashboardLayout = () => {
-    return (
-        <>
-            <div className="flex">
-                <Siderbar />
-                <Outlet />
-            </div>
-        </>
-    );
-};
+  return (
+    <>
+      <div className="flex">
+        <Siderbar />
+        <Outlet />
+      </div>
+    </>
+  )
+}
 const LandingPageLayout = () => {
-    return (
-        <>
-            {/* <DashBoardNavBar></DashBoardNavBar> */}
-            <MainNavBar></MainNavBar>
-            <Outlet></Outlet>
-            <MainFooter></MainFooter>
-            {/* <DashBoardFooter></DashBoardFooter> */}
-        </>
-    );
+  const location = useLocation()
+  const isAuthPage = location.pathname === '/login' || location.pathname === '/signup'
+  return (
+    <>
+      {/* <DashBoardNavBar></DashBoardNavBar> */}
+      {!isAuthPage && <MainNavBar />}
+      <Outlet></Outlet>
+      {!isAuthPage && <MainFooter />}
+      {/* <DashBoardFooter></DashBoardFooter> */}
+    </>
+  )
 }
 
-export { DashboardLayout, LandingPageLayout };
+export { DashboardLayout, LandingPageLayout }
