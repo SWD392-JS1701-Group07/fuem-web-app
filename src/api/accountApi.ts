ebe370a41b7bfb1fc28d69b74815d56ef2313f55
masaru05 <azusa9002@gmail.com>
2024-06-21 22:19:14 +0700

    ✨ Add getById method for account api

--- src/api/accountApi.ts
+++ src/api/accountApi.ts
@@ -1,5 +1,12 @@
-import axiosClient from "./axios";
+import { Account } from '@/constants/models/Account'
+import axiosClient from './axios'
+import axios from 'axios'
 
 export const getAccounts = async () => {
-    return await axiosClient.get("/api/accounts");
+  return await axiosClient.get('/api/accounts')
+}
+
+export const getById = async (accountId: string): Promise<Account> => {
+  const response = await axios.get(`/api/account/${accountId}`)
+  return response.data
 }
