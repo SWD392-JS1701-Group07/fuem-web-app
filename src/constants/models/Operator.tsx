057e5ab8819801d39a511c001439836bed3db87c
masaru05 <azusa9002@gmail.com>
2024-07-03 22:32:19 +0700

    ✨ Add type Operator

---
+++ src/constants/models/Operator.tsx
@@ -0,0 +1,15 @@
+export type Operator = {
+  id: number
+  name: string
+  email: string
+  username: string
+  password: string
+  studentId: string
+  phoneNumber: string
+  dob: string
+  gender: string
+  avatarUrl: string | null
+  accountStatus: string
+  roleId: number
+  subjectId: number
+}
