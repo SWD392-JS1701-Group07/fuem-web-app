7097f48c8f1a1937dd290ffa7cd18771a68114ca
masaru05 <azusa9002@gmail.com>
2024-07-03 22:41:43 +0700

    ✨ Display full details on event detail screen

--- src/pages/event/EventDetail.tsx
+++ src/pages/event/EventDetail.tsx
@@ -1,9 +1,9 @@
 import { getById } from '@/api'
 import { Button } from '@/components/ui/button'
-import { Event } from '@/constants/models/Event'
 import { AVATAR_PLACEHOLDER_URL, EVENT_PLACEHOLDER_URL } from '@/constants/models/url'
 import { formatDateTime } from '@/lib/utils'
 import * as Avatar from '@radix-ui/react-avatar'
+import { Event, type EventDetail } from '@/constants/models/Event'
 import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import EventTicket from '@/pages/event/component/EventTicket'
@@ -11,11 +11,14 @@ import EventTicket from '@/pages/event/component/EventTicket'
 const EventDetail = () => {
   const { id } = useParams<{ id: string }>()
   const [event, setEvent] = useState<Event | undefined>(undefined)
+  const [eventDetail, setEventDetail] = useState<EventDetail | undefined>(undefined)
+
   useEffect(() => {
     const getEventDetail = async () => {
       try {
         const response = await getById(parseInt(id as string))
         setEvent(response.data)
+        setEventDetail(response.data)
       } catch (error) {
         console.error('Failed to fetch event details', error)
       }
@@ -55,6 +58,7 @@ const EventDetail = () => {
           className="relative min-h-px w-full max-w-xl flex-1 bg-cover bg-center bg-no-repeat"
         >
           <h1 className="font-jura text-7xl font-extrabold">{event?.name || 'Unknown'}</h1>
+          <h1 className="font-jura text-2xl font-bold">{eventDetail?.subject.name || 'Unknown'}</h1>
           <p className="mt-6 font-jura text-4xl font-bold text-crayola">
             {schedule?.map((schedule) => (
               <div key={schedule.id}>
@@ -80,19 +84,21 @@ const EventDetail = () => {
           <Avatar.Root className="bg-blackA1 inline-flex h-40 w-40 select-none items-center justify-center overflow-hidden rounded-full align-middle">
             <Avatar.Image
               className="h-full w-full rounded-[inherit] object-cover"
-              src={AVATAR_PLACEHOLDER_URL}
+              src={eventDetail?.eventOperator.avatarUrl || AVATAR_PLACEHOLDER_URL}
               alt="Avatar"
             />
             <Avatar.Fallback
               className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
               delayMs={600}
             >
-              {event?.ownerId}
+              {eventDetail?.eventOperator.name}
             </Avatar.Fallback>
           </Avatar.Root>
           <div className="flex flex-col pl-5">
             <h3 className="font-jura text-4xl font-extrabold text-yellow-sun">EVENT ORGANIZER</h3>
-            <h2 className="pt-3 font-poppins text-2xl font-bold">{event?.ownerId}</h2>
+            <h2 className="pt-3 font-poppins text-2xl font-semibold">
+              {eventDetail?.eventOperator.name}
+            </h2>
           </div>
         </div>
         <div id="ticket" className="flex w-1/2 flex-row items-center">
@@ -103,7 +109,7 @@ const EventDetail = () => {
                 <h2 className="pt-3 font-poppins text-2xl font-bold">
                   {event?.price} <u>đ</u>
                 </h2>
-                <p className="text-xl text-gray-400">100 left</p>
+                <p className="text-xl text-gray-400">{eventDetail?.remaining} left</p>
               </div>
               <div className="flex items-center">{event && <EventTicket event={event} />}</div>
             </div>
@@ -138,39 +144,14 @@ const EventDetail = () => {
 
       <div
         id="sponsors-partners"
-        className="mx-auto flex max-w-6xl flex-wrap justify-between gap-8 p-4"
+        className="mx-auto flex max-w-6xl flex-wrap justify-center gap-8 p-4"
       >
-        <div className="flex flex-col items-center">
-          <img
-            className="h-full w-full"
-            src="https://www.elegantthemes.com/layouts/wp-content/uploads/2020/08/logo_08-white.png"
-            alt="Sponsor"
-          />
-        </div>
-
-        <div className="flex flex-col items-center">
-          <img
-            className="h-full w-full"
-            src="https://www.elegantthemes.com/layouts/wp-content/uploads/2020/08/logo_06-white.png"
-            alt="Sponsor"
-          />
-        </div>
-
-        <div className="flex flex-col items-center">
-          <img
-            className="h-full w-full"
-            src="https://www.elegantthemes.com/layouts/wp-content/uploads/2020/08/logo_05-white.png"
-            alt="Sponsor"
-          />
-        </div>
-
-        <div className="flex flex-col items-center">
-          <img
-            className="h-full w-full"
-            src="https://www.elegantthemes.com/layouts/wp-content/uploads/2020/08/logo_03-white.png"
-            alt="Sponsor"
-          />
-        </div>
+        {eventDetail?.sponsorships.map((sponsorship, index) => (
+          <div key={index} className="flex flex-col items-center">
+            <h1 className="font-jura text-4xl font-extrabold">{sponsorship.title}</h1>
+            <h3 className="pt-2 text-xl">{sponsorship.description}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
