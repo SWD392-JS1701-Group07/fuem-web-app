f29860877bb74e455d3f449600bb05c3a27264cd
masaru05 <azusa9002@gmail.com>
2024-07-03 22:23:19 +0700

    ✨ Add create order api

---
+++ src/api/orderApi.ts
@@ -0,0 +1,15 @@
+import { OrderResponse, Ticket } from '@/constants/models/Ticket'
+import axiosClient from './axios'
+
+type OrderData = {
+  orderNotes: string
+  email: string
+  phoneNumber: string
+  totalAmount: number
+  customerId: number
+  tickets: Ticket[]
+}
+
+export const createOrder = async (orderData: OrderData): Promise<OrderResponse> => {
+  return await axiosClient.post('/api/event/order/create', orderData)
+}
