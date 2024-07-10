632505765943748ce61d40c7c6532f5f6ece603a
masaru05 <azusa9002@gmail.com>
2024-07-10 21:57:54 +0700

    ✨ Add updateProfile in user API

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -1,10 +1,23 @@
-import { LoginParam } from "@/constants/models/User";
-import axiosClient from "./axios";
+import { LoginParam } from '@/constants/models/User'
+import axiosClient from './axios'
 
 export const login = async (params: LoginParam) => {
-    return await axiosClient.post("/api/auth/login", params);
+  return await axiosClient.post('/api/auth/login', params)
 }
 
 export const logout = async (param: string) => {
-    return await axiosClient.post("/api/auth/logout", param);
+  return await axiosClient.post('/api/auth/logout', param)
+}
+
+export const updateProfile = async (userId: string, formData: FormData, accessToken: string) => {
+  return await axiosClient.put(
+    `https://localhost:7297/api/accounts/update-profile/${userId}`,
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${accessToken}`
+      }
+    }
+  )
 }
