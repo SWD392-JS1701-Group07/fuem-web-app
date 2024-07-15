3e6115df4669e83bed3f93795cd40ce473afd1e6
masaru05 <azusa9002@gmail.com>
2024-07-15 23:03:26 +0700

    ✨ Add get sponsorships APIs

--- src/api/sponsorApi.ts
+++ src/api/sponsorApi.ts
@@ -1,4 +1,6 @@
+import { Sponsorships } from '@/constants/models/Event'
 import axiosClient from '../api/axios'
+import { AxiosResponse } from 'axios'
 
 export const getAll = async () => {
   return await axiosClient.get(`/api/sponsors`, {
@@ -14,16 +16,28 @@ export const getById = async (id: string) => {
 }
 
 export const searchSponsor = async (search: string) => {
-    return await axiosClient.get(`/api/sponsors`, {
-        params: {
-            searchTerm: search,
-            page: 1,
-            pageSize: 1000
-        }
-    });
+  return await axiosClient.get(`/api/sponsors`, {
+    params: {
+      searchTerm: search,
+      page: 1,
+      pageSize: 1000
+    }
+  })
 }
 
-export const getSponsorById = async (id: number) => {
-    return await axiosClient.get(`/api/sponsors/${id}`)
+export const getSponsorshipsById = async (id: number) => {
+  return await axiosClient.get(`/api/sponsorships/${id}`)
 }
 
+export const getSponsorshipsBySponsorId = async (
+  accountId: number,
+  search?: string
+): Promise<AxiosResponse<Sponsorships[]>> => {
+  return await axiosClient.get(`/api/sponsorships/sponsors/${accountId}`, {
+    params: {
+      searchTerm: search,
+      page: 1,
+      pageSize: 1000
+    }
+  })
+}
