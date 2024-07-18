0166800c1c48d0a62a96c4d2c013d0c5797f08c3
masaru05 <azusa9002@gmail.com>
2024-07-18 11:17:38 +0700

    ✨ Add `getOrders` and `getOrderById` API calls

--- src/api/orderApi.ts
+++ src/api/orderApi.ts
@@ -9,6 +9,22 @@ type OrderData = {
   tickets: Ticket[]
 }
 
+export const getOrders = async (params?: {
+  isBought?: boolean
+  searchTerm?: string
+  email?: string
+}) => {
+  const config = {
+    params: params
+  }
+
+  return await axiosClient.get('/orders', config)
+}
+
+export const getOrderById = async (id: string) => {
+  return await axiosClient.get(`/orders/${id}`)
+}
+
 export const createOrder = async (orderData: OrderData): Promise<OrderResponse> => {
   return await axiosClient.post('/api/event/order/create', orderData)
 }
