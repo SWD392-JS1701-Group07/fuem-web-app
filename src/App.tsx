15218f1ecc201ec74f22e2c13134e9c6c629bfab
masaru05 <azusa9002@gmail.com>
2024-07-18 11:22:31 +0700

    ✨ Update routers for order dashboard pages

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,8 @@ import SponsorDashboardDetail from './pages/dashboard/sponsor/component/SponsorD
 import SponsorshipList from './pages/dashboard/sponsorship/SponsorshipList'
 import SponsorshipDashboardDetail from './pages/dashboard/sponsorship/component/SponsorshipDashboardDetail'
 import SponsorshipEdit from './pages/dashboard/sponsorship/component/SponsorshipEdit'
+import OrderList from './pages/dashboard/order/OrderList'
+import OrderDashboardDetail from './pages/dashboard/order/component/OrderDashboardDetail'
 
 function App() {
   //const { component: Component, path } = routes[3];
@@ -84,6 +86,8 @@ function App() {
             <Route path="sponsorship" element={<SponsorshipList />} />
             <Route path="sponsorship/:id" element={<SponsorshipDashboardDetail />} />
             <Route path="sponsorship/:id/edit" element={<SponsorshipEdit />} />
+            <Route path="order" element={<OrderList />} />
+            <Route path="order/:id" element={<OrderDashboardDetail />} />
             <Route path="operator" element={<OperatorList />} />
             <Route path="operator/:id" element={<OperatorDashboardDetail />} />
             <Route path="visitor" element={<VisitorList />} />
